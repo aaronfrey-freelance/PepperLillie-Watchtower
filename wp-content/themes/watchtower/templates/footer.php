c8b0b7af73fb192398078861c4f6f335bbdb90a8
Aaron Frey <aaron.frey@gmail.com>
2017-10-25 23:33:09 -0400

    add social options to admin

--- wp-content/themes/watchtower/templates/footer.php
+++ wp-content/themes/watchtower/templates/footer.php
@@ -1,19 +1,28 @@
-<footer class="content-info">
+<div class="social-container">
 
-  <div class="container">
-
-  	<div class="social clearfix">
-  		<a href="#">
+	<div class="social">
+		<?php $facebook_account = get_field('facebook_account', 'option');
+		if ($facebook_account) : ?>
+			<a href="<?php echo $facebook_account; ?>" target="_blank">
 				<i class="fa fa-facebook"></i>
-  		</a>
-  		<a href="#">
+			</a>
+		<?php endif; ?>
+		<?php $instagram_account = get_field('instagram_account', 'option');
+		if ($instagram_account) : ?>
+			<a href="<?php echo $instagram_account; ?>" target="_blank">
 				<i class="fa fa-instagram"></i>		
-  		</a>
-  		<a href="#">
+			</a>
+		<?php endif; ?>
+		<?php $twitter_account = get_field('twitter_account', 'option');
+		if ($twitter_account) : ?>
+			<a href="<?php echo $twitter_account; ?>" target="_blank">
 				<i class="fa fa-twitter"></i>		
-  		</a>
-  	</div>
+			</a>
+		<?php endif; ?>
+	</div>
 
-  </div>
+</div>
 
-</footer>
+<!-- <footer class="content-info">
+  <div class="container"></div>
+</footer> -->
